c22bbe691c900f6380be21552c094889bef26840
Gruruya <gruruya.chi4c@slmails.com>
2023-06-05 11:22:35 -0400

    Add case insensitive finding/smart case
    (cherry picked from commit 4507de3f9014a5fdc9994436fdce064b33c68ec5)

--- src/f/find.nim
+++ src/f/find.nim
@@ -30,15 +30,44 @@ proc findOA*(text, pattern: openArray[char], start = 0): int {.inline.} =
       if matches: return i
   result = -1
 
+func toLowerAscii(c: char): char {.inline.} =
+  if c in {'A'..'Z'}:
+    char(uint8(c) xor 0b0010_0000'u8)
+  else: c
+
+proc cmpInsensitive(a, b: char): bool {.inline.} =
+  a.toLowerAscii == b.toLowerAscii
+
+proc findOAI*(text, pattern: openArray[char], start = 0): int {.inline.} =
+  for i in start..text.len - pattern.len:
+    if cmpInsensitive(text[i], pattern[0]):
+      var matches = true
+      for i2 in 1 ..< pattern.len:
+        if not cmpInsensitive(text[i + i2], pattern[i2]):
+          matches = false
+          break
+      if matches: return i
+  result = -1
+
 template find*(text, pattern: string, start = 0): int =
   findOA(text, pattern, start)
 
+template findI*(text, pattern: string, start = 0): int =
+  findOAI(text, pattern, start)
+
 proc find*(text: openArray[char], patterns: seq[string]): seq[int] =
+  var sensitive = false
+  block smartCase:
+    for s in patterns:
+      for c in s:
+        if c in {'A'..'Z'}:
+          sensitive = true
+          break smartCase
   result = newSeqOfCap[int](patterns.len)
   var start = 0
   for pattern in patterns:
     if start > text.high: return @[]
-    let where = text.findOA(pattern, start)
+    let where = if sensitive: text.findOA(pattern, start) else: text.findOAI(pattern, start)
     if where == -1: return @[]
     result.add where
     start = where + pattern.len
