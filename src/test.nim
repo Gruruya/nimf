e2ad1a9b7df16fe0e9da90c4a777e0dffa03d439
Gruruya <gruruya.chi4c@slmails.com>
2023-06-05 11:25:03 -0400

    Delete debug file
    (cherry picked from commit c8712f7883d3d1bbe4f110908c5640d27d19cd42)

--- src/test.nim
+++
@@ -1,9 +0,0 @@
-import ./f
-import pkg/criterion
-import strutils
-
-var cfg = newDefaultConfig()
-
-benchmark cfg:
-  proc findba() {.measure.} =
-    blackBox f.find("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaA", "ba")
