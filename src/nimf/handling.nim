8ecf60cdcd9ae40dfbc072b72223f6a1ca745562
Gruruya <gruruya.chi4c@slmails.com>
2023-07-10 23:54:41 -0400

    * src/nimf/handling.nim (replaceAt): Less temporaries, less clear

--- src/nimf/handling.nim
+++ src/nimf/handling.nim
@@ -88,13 +88,23 @@ func kwayMerge[T: Ordinal](seqOfSeqs: openArray[seq[T]]): seq[(T, Natural)] =
       result.add (seqOfSeqs[minIdx][indices[minIdx]], minIdx)
       inc indices[minIdx]
 
-func replaceAt(text: string; placements: openArray[tuple[where, which: Natural]]; replacements: openArray[tuple[sub, by: string]]): string =
+func replaceAt[T: enum](text: string; placements: openArray[tuple[where: Natural, which: Natural]]; replacements: array[T, string]): string =
   ## Replaces at each `placements.where` index the `replacements[placements.which].sub` text with `by`
   var start = text.low
   for target in placements:
     result &= text[start ..< target.where]
-    result &= replacements[target.which].by
-    start = target.where + replacements[target.which].sub.len
+    result &= replacements[cast[T](target.which)]
+    start = target.where + (cast[T](target.which)).`$`.len
+  if start <= text.high:
+    result &= text[start .. text.high]
+
+func replaceAt[T: enum](text: string; placements: openArray[tuple[where: Natural, which: Natural]]; replacements: array[T, seq[string]]; index: Natural): string =
+  ## Replaces at each `placements.where` index the `replacements[placements.which].sub` text with `by`
+  var start = text.low
+  for target in placements:
+    result &= text[start ..< target.where]
+    result &= replacements[cast[T](target.which)][index]
+    start = target.where + (cast[T](target.which)).`$`.len
   if start <= text.high:
     result &= text[start .. text.high]
 
@@ -110,22 +120,26 @@ proc run*(cmds: sink seq[string], findings: seq[Found]) =
   const Targets = (proc(): array[Target.enumLen, string] =
                      for t in Target: result[ord(t)] = $t)()
 
-  var replacementsStored: array[Target, seq[string]]
-  var replacementsJoinedStored: array[Target, string]
+  var replacements: array[Target, seq[string]]
+  var replacementsJoined: array[Target, string]
 
-  template needs[T](variable: var T, constructor: T): T =
-    if variable.len == 0: variable = constructor
-    variable
-  template getReplacement(t: Target; findings: seq[Found]): seq[string] =
-    needs(replacementsStored[t],
+  template needs(t: Target, constructor: typed) =
+    if replacements[t].len == 0: replacements[t] = constructor
+
+  template needs(t: Target; findings: seq[Found]) =
+    needs(t,
       case t
       of toPaths: mapIt(findings, it.path.string.quoteShell)
       of toFilenames: mapIt(findings, it.path.lastPathPart.string.quoteShell)
       of toParentDirs: mapIt(findings, it.path.parentDir.string.quoteShell)
       of toNoExtPaths: mapIt(findings, it.path.stripExtension.string.quoteShell)
-      of toNoExtFilenames: mapEnumeratedIt(findings, if it.kind == pcDir: needs(replacementsStored[toFilenames], mapIt(findings, it.path.lastPathPart.string.quoteShell))[i] else: it.path.splitFile[1].string.quoteShell))
-  template getReplacementJoined(t: Target; findings: seq[Found]): string =
-    needs(replacementsJoinedStored[t], getReplacement(t, findings).join(" "))
+      of toNoExtFilenames: mapEnumeratedIt(findings, if it.kind == pcDir: needs(toFilenames, mapIt(findings, it.path.lastPathPart.string.quoteShell)); replacements[toFilenames][i]
+                                                     else: it.path.splitFile[1].string.quoteShell))
+
+  template needsJoined(t: Target; findings: seq[Found]) =
+    if replacementsJoined[t].len == 0:
+      needs(t, findings)
+      replacementsJoined[t] = replacements[t].join(" ")
 
   proc execShell(cmd: string) = discard execShellCmd(cmd)
   var m = createMaster()
@@ -136,25 +150,19 @@ proc run*(cmds: sink seq[string], findings: seq[Found]) =
       if cmd.endsWith "+":
         cmd.setLen(cmd.len - 1)
         var anyPlaceholders = false
-        var replacementPairs = static:
-          (proc: array[Target.enumLen, (string, string)] =
-            for i in 0..Targets.high: result[i][0] = Targets[i])()
         for t in Target:
           if allIndexes[ord(t)].len > 0:
-            replacementPairs[ord(t)][1] = getReplacementJoined(t, findings)
+            needsJoined(t, findings)
             anyPlaceholders = true
         if not anyPlaceholders:
-              m.spawn execShell cmd & ' ' & getReplacementJoined(toPaths, findings)
-        else: m.spawn execShell cmd.replaceAt(placements, replacementPairs)
+              m.spawn execShell cmd & ' ' & (needsJoined(toPaths, findings); replacementsJoined[toPaths])
+        else: m.spawn execShell cmd.replaceAt(placements, replacementsJoined)
       else:
         let anyPlaceholders = anyIt(allIndexes, it.len > 0)
         for i in findings.low..findings.high:
-          var replacementPairs = static:
-            (proc: array[Target.enumLen, (string, string)] =
-              for i in 0..Targets.high: result[i][0] = Targets[i])()
           for t in Target:
             if allIndexes[ord(t)].len > 0:
-              replacementPairs[ord(t)][1] = getReplacement(t, findings)[i]
+              needs(t, findings)
           if not anyPlaceholders:
-                m.spawn execShell cmd & ' ' & getReplacement(toPaths, findings)[i]
-          else: m.spawn execShell cmd.replaceAt(placements, replacementPairs)
+                m.spawn execShell cmd & ' ' & (needs(toPaths, findings); replacements[toPaths][i])
+          else: m.spawn execShell cmd.replaceAt(placements, replacements, i)
