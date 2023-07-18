09045bcb66a5b4227ed12201b232767914b5cb5f
Gruruya <gruruya.chi4c@slmails.com>
2023-07-17 21:55:33 -0400

    Use moRelaxed for load

--- src/nimf/findFiles.nim
+++ src/nimf/findFiles.nim
@@ -226,7 +226,7 @@ proc notFoundPrint() =
 {.pop inline.}
 
 proc findDirRec(m: MasterHandle; dir: Path; patterns: openArray[string]; kinds: set[PathComponent]; followSymlinks: bool; behavior: RunOption; depth: Positive) {.gcsafe.} =
-  if behavior.maxFound != 0 and numFound.load() >= behavior.maxFound: return
+  if behavior.maxFound != 0 and numFound.load(moRelaxed) >= behavior.maxFound: return
 
   template loop: untyped =
     template format(path: string): Path =
